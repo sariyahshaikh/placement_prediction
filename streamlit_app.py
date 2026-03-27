09aa0163913339042590fcbb1f8604e7b66634df
sariyahshaikh <136481994+sariyahshaikh@users.noreply.github.com>
2026-03-27 12:08:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ projects=st.slider("Projects",0,5,2)
 
 if st.button("Predict"):
     url="http://127.0.0.1:5000/predict"
+    url="https://placement-prediction-1-adty.onrender.com/predict"
 
     data={
         "cgpa":cgpa,
